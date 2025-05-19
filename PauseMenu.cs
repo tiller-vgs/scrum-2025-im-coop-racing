c323fa48ca1ce99309d3dbca929a9ae0176aac42
Tobythefirst1 <154324582+Tobythefirst1@users.noreply.github.com>
2025-05-19 08:50:17 +0200

    I have started on the pause menu script.
    ref: #11

---
+++ PauseMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool PauseActive = false;
+
+    public GameObject pauseMenuObject
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            
+        }
+    }
+}
