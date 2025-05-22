f15e5bd21b998562327122c46394d9f77ba6042d
Tobythefirst1 <154324582+Tobythefirst1@users.noreply.github.com>
2025-05-22 12:09:56 +0200

    The code for the pause menu is finished
    #3

--- PauseMenu.cs
+++ PauseMenu.cs
@@ -7,10 +7,43 @@ public class PauseMenu : MonoBehaviour
 
     public GameObject pauseMenuObject
 
+
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            
+            if (PauseIsActive)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
         }
+    }
+
+    public void Resume()
+    {
+        pauseMenuObject.SetActive(false);
+        Time.timeScale = 1f;
+        PauseIsActive = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuObject.SetActive(true);
+        Time.timeScale = 0f;
+        PauseIsActive = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("1.MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting game");
     }
 }
