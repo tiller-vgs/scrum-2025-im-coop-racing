c9d3065726e77015c43267b3195ca8c36d5e7e0e
Tobythefirst1 <154324582+Tobythefirst1@users.noreply.github.com>
2025-05-23 10:08:18 +0200

    Finished the code for the pause menu, also linked it to the time limit script. #3

--- PauseMenu.cs
+++ PauseMenu.cs
@@ -1,49 +1,50 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Timer.cs;
 
 public class PauseMenu : MonoBehaviour
 {
-    public static bool PauseActive = false;
+    public static bool PauseActive = false; //the application will always start with this variable set as false
 
-    public GameObject pauseMenuObject
+    public GameObject pauseMenuObject //declares which unity object is connected to this script, so the object gets turned off in unity, and vice versa.
 
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape)) //if the escape key is pressed
         {
-            if (PauseIsActive)
+            if (PauseIsActive) //if the static bool is true
             {
-                Resume();
+                Resume(); 
             }
-            else
+            else //if the static bool is false (anything other than true)
             {
                 Pause();
             }
         }
     }
 
-    public void Resume()
+    public void Resume() //the code that unpauses the application
     {
-        pauseMenuObject.SetActive(false);
-        Time.timeScale = 1f;
+        pauseMenuObject.SetActive(false); //turns off the object(s) connected to this script when false
+        TimerIsRunning == true;
         PauseIsActive = false;
     }
 
-    void Pause()
+    void Pause() //the code that "pauses" the application
     {
-        pauseMenuObject.SetActive(true);
-        Time.timeScale = 0f;
+        pauseMenuObject.SetActive(true); //turns on the unity objects connected to this script when true
+        TimerIsRunning == false;
         PauseIsActive = true;
     }
 
-    public void LoadMenu()
+    public void LoadMenu() //this code loads a unity scene when read
     {
-        SceneManager.LoadScene("1.MainMenu");
+        SceneManager.LoadScene("1.MainMenu"); //loads the scene that is the main menu. 
     }
 
-    public void QuitGame()
+    public void QuitGame() //quits the application if read
     {
-        Application.Quit();
-        Debug.Log("Quitting game");
+        Application.Quit(); //quits the application
+        Debug.Log("Quitting game"); //writes a message visible when the application is in debug
     }
 }
